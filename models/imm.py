d0018049c0f453f997d4ed3fa6052d9a8a58f1c2
BravoChaoS <43561142+BravoChaoS@users.noreply.github.com>
2020-11-28 02:14:57 +0800

    static theta final

--- models/imm.py
+++ models/imm.py
@@ -1,4 +1,3 @@
-import math
 import time
 
 from numpy import random
@@ -14,7 +13,6 @@ class IMM:
         self.l = 0
         self.n = 0
         self.k = 0
-        self.log_cnk = 0
         self.tl = 60
         self.ts = 0
         self.te = 0
@@ -27,29 +25,18 @@ class IMM:
         self.l = l
         self.n = n
         self.k = k
-        self.log_cnk = 0
         self.tl = tl
         self.ts = time.time()
-        for i in range(n - k + 1, n + 1):
-            self.log_cnk += math.log(i)
-        for i in range(1, k + 1):
-            self.log_cnk -= math.log(i)
 
         rrs = self.sampling()
         # print(len(rrs))
-        seeds, size = self.node_selection(rrs)
+        seeds = self.node_selection(rrs)
         return seeds
-
-    def get_lambda_aster(self):
-        alpha = math.sqrt(self.l * math.log(self.n) + math.log(2))
-        beta = math.sqrt((1 - 1 / math.e) * (self.log_cnk + self.l * math.log(self.n) + math.log(2)))
-        return 2 * self.n * pow(((1 - 1 / math.e) * alpha + beta), 2) * pow(self.e, -2)
 
     def sampling(self):
         rrs = []
         tlim = self.tl * 0.8
 
-        random.seed(int(time.time()))
         s1 = time.time()
         while time.time() - s1 < tlim and len(rrs) < 5000000:
             v = random.randint(1, self.n)
@@ -60,32 +47,9 @@ class IMM:
 
         # print('sampling', time.time() - s1)
         # print('rrs_len', len(rrs))
-
-        # ep = math.sqrt(2) * self.e
-        # rnd = int(math.log2(self.n - 1)) + 1
-        # for i in range(1, rnd):
-        #     x = self.n / math.pow(2, i)
-        #     lp = ((2 + 2 * ep / 3) * (
-        #             self.log_cnk + self.l * math.log(self.n) + math.log(math.log2(self.n))) * self.n) / pow(ep, 2)
-        #     theta = lp / x
-        #
-        #     rrs += self.generate_rrs(theta - len(rrs))
-        #     seeds, f = self.node_selection(rrs)
-        #
-        #     if self.n * f >= (1 + ep) * x:
-        #         self.lb = self.n * f / (1 + ep)
-        #         break
-        #
-        # ls = self.get_lambda_aster()
-        # theta = ls / self.lb
-        #
-        # if len(rrs) < theta:
-        #     rrs += self.generate_rrs(theta - len(rrs))
-
         return rrs
 
     def node_selection(self, rrs):
-        s2 = time.time()
         seeds = []
         node_rr = [[] for i in range(self.n + 1)]
         node_rr_len = [0 for i in range(self.n + 1)]
@@ -105,16 +69,8 @@ class IMM:
                     vis_rr[j] = False
                     for nd in rrs[j]:
                         node_rr_len[nd] -= 1
-
-        cnt = 0
-
-        for i in range(len(rrs)):
-            if not vis_rr[i]:
-                cnt += 1
-
-        # print(len(rrs), len(seed_rr) / len(rrs))
         # print('node_selection', time.time() - s2)
-        return seeds, cnt / len(rrs)
+        return seeds
 
     def generate_rr_ic(self, v):
         activated = [v]
@@ -142,8 +98,6 @@ class IMM:
             if len(adj) == 0:
                 break
             new_activated = adj[random.randint(0, len(adj))][0]
-            if new_activated in visited:
-                break
             visited.add(new_activated)
             activated = new_activated
         return visited
